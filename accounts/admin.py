8adb069513f770779f543d0ea952cd1e62ad6cdd
baqer marani <60050143+baqermarani@users.noreply.github.com>
2022-04-26 18:17:45 +0430

    Register My model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserUpdateForm
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    form = UserUpdateForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'is_admin',)
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'phone_number', 'full_name' ,'password')}),
+        ('Permissions', {'fields': ('is_admin','is_active','last_login')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'phone_number', 'full_name', 'password1', 'password2')}),
+    )
+    search_fields = ('email','full_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin) # Register the UserAdmin class
